e78e7c4cc7521852cdb981aaca4a0a9c338de382
HambokJiSu <truemindy@gmail.com>
2022-04-07 00:44:24 +0900

    Updates

---
+++ precache-manifest.7d322b366965dbb90ece4cd5f50ec022.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7556b9dc00ff3ff54e6c",
+    "url": "/react_moive/static/css/main.ad3133bf.chunk.css"
+  },
+  {
+    "revision": "7556b9dc00ff3ff54e6c",
+    "url": "/react_moive/static/js/main.7556b9dc.chunk.js"
+  },
+  {
+    "revision": "ecef472346c0fa57e759",
+    "url": "/react_moive/static/js/1.ecef4723.chunk.js"
+  },
+  {
+    "revision": "3563bbfa651f6054936a",
+    "url": "/react_moive/static/js/runtime~main.3563bbfa.js"
+  },
+  {
+    "revision": "e4143ba7f9c04cc8eda0179206d4c44b",
+    "url": "/react_moive/index.html"
+  }
+];
